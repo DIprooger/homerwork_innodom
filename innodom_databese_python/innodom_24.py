b88edd163dc02d0100fbe9667af88c1af876b873
DIprooger <dddddd>
2023-12-17 00:47:05 +0300

    create 24

--- innodom_databese_python/innodom_24.py
+++ innodom_databese_python/innodom_24.py
@@ -7,3 +7,41 @@
 # - выберите книги, у которых цена выше определенного значения.
 # - обновите цену книги с определенным ID.
 # - удалите книги, у которых год издания меньше определенного значения
+
+
+import psycopg2
+import environ
+import os
+from urllib.parse import quote
+
+BASE_DIR = '/home/diana/Desktop/Python/тренировка/homerwor_programing_innodom/innodom_databese_python'
+
+env = environ.Env(frame=None)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+connection = psycopg2.connect(
+    dbname=env('DB_NAME_POS'),
+    user=env('DB_USER_POS'),
+    password=env('DB_PASSWORD_POS'),
+    host=env('DB_HOST_POS'),
+    port=env('DB_PORT_POS')
+)
+
+# connection.connect()
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Books(
+        id INT PRIMARY KEY,
+        title VARCHAR(50),
+        author VARCHAR(30),
+        year INT,
+        price INT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT NULL       
+    )
+""")
+
+connection.commit()
+cursor.close()
+connection.close()
