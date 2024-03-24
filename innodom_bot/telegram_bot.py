051dbbe9e7906a3cbea0aa58f0b3eaef7b832748
DIprooger <dddddd>
2024-03-24 10:00:02 +0300

    add: database

--- innodom_bot/telegram_bot.py
+++ innodom_bot/telegram_bot.py
@@ -1,10 +1,51 @@
 import telebot
 import re
-from telebot import types
+import environ
+import os
+import psycopg2
+from pathlib import Path
 
-TOKEN = '6935628256:AAG7WDhCF42l_zEDt4u173ovb3-ECk3qoTc'
+BASE_DIR = '/home/diana/Desktop/Python/тренировка/homerwor_programing_innodom/innodom_bot'
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+connection = psycopg2.connect(
+    dbname=env('DB_NAME_POS'),
+    user=env('DB_USER_POS'),
+    password=env('DB_PASSWORD_POS'),
+    host=env('DB_HOST_POS'),
+    port=env('DB_PORT_POS')
+)
+
+TOKEN = env('TOKEN')
 bot = telebot.TeleBot(TOKEN)
+
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Bot(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(225),
+    query_text VARCHAR(225),
+    response_text VARCHAR(225),
+    query_time TIMESTAMP DEFAULT NOW()
+    );
+""")
+connection.commit()
+
+
+def insert_query_into_database(name, query_text, response_text):
+    try:
+        cursor.execute(f"""
+                INSERT INTO Bot(name, query_text, response_text)
+                VALUES
+                    (%s, %s, %s);
+            """, (name, query_text, response_text))
+        connection.commit()
+
+    except Exception as e:
+        print(e)
 
 
 @bot.message_handler(commands=['start'])
@@ -14,16 +55,21 @@ def start(n):
 
 @bot.message_handler(func=lambda message: True)
 def calculate(message):
+    user_name = message.from_user.username
+    expression_text = message.text
     try:
-        expression = message.text
-        expression = re.sub(r'[^\d\+\-\*\/\(\)\.\,]', '', expression)
-
+        expression = re.sub(r'[^\d\+\-\*\/\(\)\.\,]', '', expression_text)
         result = eval(expression)
-
         bot.reply_to(message, f"Результат: {result}")
+        insert_query_into_database(user_name, expression, result)
+
     except Exception as e:
-        bot.reply_to(message, "Произошла ошибка при вычислении. Убедитесь, что введено корректное математическое "
-                              "выражение.")
+        result = "Произошла ошибка при вычислении. Убедитесь, что введено корректное математическое выражение."
+        bot.reply_to(message, result)
+        insert_query_into_database(user_name, expression_text, 'Ошибка')
+
 
 
 bot.polling()
+cursor.close()
+connection.close()
